b3efdb95bac56a2910d7d58cecba91a67c9325fb
yangyuexiong <z87254091>
2020-12-21 11:12:51 +0800

    RBAC模型的用户与权限设计

--- app/models/admin/models.py
+++ app/models/admin/models.py
@@ -10,62 +10,33 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from ExtendRegister.db_register import db
 from common.libs.BaseModel import BaseModel
 
+"""
+RBAC
+用户-角色-权限
+"""
 
-# 权限
-class CMSPersmission(object):
-    # 255的二进制方式来表示 1111 1111
-    ALL_PERMISSION = 0b11111111
-    # 1. 访问者权限
-    VISITOR = 0b00000001
-    # 2. 管理帖子权限
-    POSTER = 0b00000010
-    # 3. 管理评论的权限
-    COMMENTER = 0b00000100
-    # 4. 管理板块的权限
-    BOARDER = 0b00001000
-    # 5. 管理前台用户的权限
-    FRONTUSER = 0b00010000
-    # 6. 管理后台用户的权限
-    CMSUSER = 0b00100000
-    # 7. 管理后台管理员的权限
-    ADMINER = 0b01000000
-
-
-cms_role_user = db.Table(
-    'cms_role_user',  # 表名
-    # 字段名:cms_role_id,
-    # 类型
-    # 外键:cms_role.id,
-    # 设置为主键
-    db.Column('cms_role_id', db.Integer, db.ForeignKey('cms_role.id'), primary_key=True, comment='权限id'),
-    db.Column('cms_user_id', db.Integer, db.ForeignKey('cms_user.id'), primary_key=True, comment='用户id'),
+crm_user_and_role = db.Table(
+    'cms_user_and_role',
+    db.Column('cms_user_id', db.Integer, db.ForeignKey('crm_user.id'), primary_key=True, comment='用户id'),
+    db.Column('cms_role_id', db.Integer, db.ForeignKey('crm_role.id'), primary_key=True, comment='角色id'),
     comment='用户_角色_中间表'
 )
 
-
-# CMS角色
-class CMSRole(BaseModel):
-    __tablename__ = 'cms_role'
-    __table_args__ = {'comment': '权限角色表'}
-    name = db.Column(db.String(50), nullable=False, comment='权限名称')
-    desc = db.Column(db.String(200), nullable=True, comment='权限描述')
-    # create_time = db.Column(db.DateTime, default=datetime.now)
-    permissions = db.Column(db.Integer, default=CMSPersmission.VISITOR, comment='权限等级')
-
-    # 引用模型:CMSUser
-    # 中间表:cms_role_user
-    # 通过CMSUser反向引用:roles
-    users = db.relationship('CMSUser', secondary=cms_role_user, backref='roles')
+crm_permission_and_role = db.Table(
+    'cms_permission_and_role',
+    db.Column('cms_permission_id', db.Integer, db.ForeignKey('crm_permission.id'), primary_key=True, comment='权限id'),
+    db.Column('cms_role_id', db.Integer, db.ForeignKey('crm_role.id'), primary_key=True, comment='角色id'),
+    comment='权限_角色_中间表'
+)
 
 
-# CMS用户
-class CMSUser(BaseModel):
-    __tablename__ = 'cms_user'
-    __table_args__ = {'comment': '用户表'}
+class Admin(BaseModel):
+    __tablename__ = 'crm_user'
+    __table_args__ = {'comment': '后台用户表'}
     username = db.Column(db.String(50), nullable=False, comment='账号')
     _password = db.Column(db.String(100), nullable=False, comment='密码')
-
-    # join_time = db.Column(db.DateTime, server_default=db.func.now())
+    mail = db.Column(db.String(100), nullable=True, comment='邮箱')
+    remark = db.Column(db.String(255), nullable=True, comment='备注')
 
     def __init__(self, username, password):
         self.username = username
@@ -83,28 +54,28 @@ class CMSUser(BaseModel):
         result = check_password_hash(self.password, raw_password)
         return result
 
-    @property
-    def permissions(self):
-        if not self.roles:
-            return 0
-        all_permissions = 0
-        for role in self.roles:
-            permissions = role.permissions
-            all_permissions |= permissions  # 整合该用户所有权限
-            # print('权限包含：', all_permissions)
-        return all_permissions
+    def __repr__(self):
+        return 'Admin 模型对象-> ID:{} 用户名:{}'.format(self.id, self.username)
 
-    # 是否拥有权限
-    def has_permission(self, permission):
-        # all_permissions = self.permissions
-        # result = all_permissions&permission == permission
-        # return result
-        return self.permissions & permission == permission
 
-    # 开发者
-    @property
-    def is_developer(self):
-        return self.has_permission(CMSPersmission.ALL_PERMISSION)
+class Role(BaseModel):
+    __tablename__ = 'crm_role'
+    __table_args__ = {'comment': '后台角色表'}
+    name = db.Column(db.String(50), nullable=False, comment='角色名称')
+    remark = db.Column(db.String(255), nullable=True, comment='备注')
+
+    user_list = db.relationship('Admin', secondary=crm_user_and_role, backref='roles')
+    permission_list = db.relationship('Permission', secondary=crm_permission_and_role, backref='roles')
 
     def __repr__(self):
-        return 'admin模型对象-> 用户名:{} 密码:{}'.format(self.username, self.password)
+        return 'Role 模型对象-> ID:{} 角色名称:{}'.format(self.id, self.name)
+
+
+class Permission(BaseModel):
+    __tablename__ = 'crm_permission'
+    __table_args__ = {'comment': '后台权限表'}
+    name = db.Column(db.String(50), nullable=False, comment='权限名称')
+    remark = db.Column(db.String(255), nullable=True, comment='备注')
+
+    def __repr__(self):
+        return 'Permission 模型对象-> ID:{} 权限名称:{}'.format(self.id, self.name)
--- manage.py
+++ manage.py
@@ -12,22 +12,17 @@ from flask_migrate import Migrate, MigrateCommand
 
 from ApplicationExample import create_app
 from ExtendRegister.db_register import db
-from app.models.admin import models as cms_models
+from app.models.admin.models import Admin, Role, Permission
 
 app = create_app()  # 实例
 manager = Manager(app)  # 绑定
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)  # 添加命令
 
-# CMS
-CMSUser = cms_models.CMSUser
-CMSRole = cms_models.CMSRole
-CMSPermission = cms_models.CMSPersmission
-
 
 # 自定义命令一：
 class Hello(Command):
-    'hello world'
+    """hello world"""
 
     def run(self):
         print('hello world')
@@ -58,63 +53,49 @@ class TableCreate(Command):
             print('创建失败:{}'.format(str(e)))
 
 
+class CRMInit(Command):
+    """cms初始化"""
+
+    def run(self):
+        try:
+            user = Admin.query.filter_by(username='admin').first()
+            if user:
+                pass
+            else:
+                user = Admin(username='admin', password='123456')
+                db.session.add(user)
+                db.session.commit()
+                print('cms用户添加成功')
+            for r in range(1, 5):
+                role_obj = Role(name='角色{}'.format(str(r)))
+                db.session.add(role_obj)
+            db.session.commit()
+            print('cms角色添加成功')
+
+            for p in range(1, 5):
+                permission_obj = Permission(name='权限{}'.format(str(p)))
+                db.session.add(permission_obj)
+            db.session.commit()
+            print('cms权限添加成功')
+        except BaseException as e:
+            print('出错:{}'.format(str(e)))
+
+
 # 添加命令
 manager.add_command('hello', Hello())
 manager.add_command('orm', TableCreateFirst())
 manager.add_command('table', TableCreate())
-
-
-# CMS权限
-@manager.command
-def create_role():
-    """创建后台管理角色"""
-    # 1. 酱油小伙子
-    visitor = CMSRole(name='酱油小伙子', desc='Lv===1')
-    visitor.permissions = CMSPermission.VISITOR
-
-    # 2. 普通管理员（修改个人个人信息，管理帖子，管理评论，管理前台用户）
-    operator = CMSRole(name='运营', desc='Lv===33')
-    operator.permissions = CMSPermission.VISITOR | CMSPermission.POSTER | CMSPermission.CMSUSER | CMSPermission.COMMENTER | CMSPermission.FRONTUSER
-
-    # 3. 管理员（拥有绝大部分权限）
-    admin = CMSRole(name='管理员', desc='Lv===88')
-    admin.permissions = CMSPermission.VISITOR | CMSPermission.POSTER | CMSPermission.CMSUSER | CMSPermission.COMMENTER | CMSPermission.FRONTUSER | CMSPermission.BOARDER
-
-    # 4. 博主
-    developer = CMSRole(name='博主', desc='Lv===99')
-    developer.permissions = CMSPermission.ALL_PERMISSION
-
-    db.session.add_all([visitor, operator, admin, developer])
-    db.session.commit()
-    print('角色创建完成')
+manager.add_command('crm', CRMInit())
 
 
 @manager.option('-u', '--username', dest='username')
 @manager.option('-p', '--password', dest='password')
 def create_cms_user(username, password):
     """创建后台管理用户"""
-    user = CMSUser(username=username, password=password)
+    user = Admin(username=username, password=password)
     db.session.add(user)
     db.session.commit()
     print('cms用户添加成功')
-
-
-# 赋予用户权限
-@manager.option('-u', '--username', dest='username')
-@manager.option('-n', '--name', dest='name')
-def add_user_to_role(username, name):
-    """为角色添加权限"""
-    user = CMSUser.query.filter_by(username=username).first()
-    if user:
-        role = CMSRole.query.filter_by(name=name).first()
-        if role:
-            role.users.append(user)
-            db.session.commit()
-            print('用户添加到角色成功！')
-        else:
-            print('没有这个角色：%s' % role)
-    else:
-        print('用户:%s不存在!' % username)
 
 
 # 自定义命令二：
@@ -154,11 +135,12 @@ if __name__ == '__main__':
     #   ps:先备份数据
     #       python3 manage.py db history
     #       python3 manage.py db downgrade id
-    '''CMS角色'''
-    # 创建后台管理角色:python3 manage.py create_role
+
+    '''库表初始化'''
+    # python3 manage.py orm
+
+    '''cms初始化'''
+    # python3 manage.py crm
+
     '''CMS用户'''
     # 创建后台管理用户:python3 manage.py create_cms_user -u yyx -p 666666
-    '''权限'''
-    # 为角色添加权限:python3 manage.py add_user_to_role -u yyx -n 博主
-    '''测试'''
-    # 测试权限控制:python3 manage.py test_permission
