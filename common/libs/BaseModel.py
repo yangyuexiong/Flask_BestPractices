6cbc839f0c0d23c7ee5940a6cb3d5394fe53c16a
yangyuexiong <yang6333yyx@126.com>
2021-12-14 15:05:03 +0800

    feat:优化BaseModel

--- common/libs/BaseModel.py
+++ common/libs/BaseModel.py
@@ -28,6 +28,8 @@ class BaseModel(db.Model):
     status:状态
     """
 
+    hidden_fields = []  # 不需要返回的字段与值
+
     __abstract__ = True
 
     id = db.Column(BIGINT(20, unsigned=True), primary_key=True, autoincrement=True, comment='id')
@@ -35,44 +37,48 @@ class BaseModel(db.Model):
     create_timestamp = db.Column(BIGINT(20, unsigned=True), default=int(time.time()), comment='创建时间(时间戳)')
     update_time = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, comment='更新时间(结构化时间)')
     update_timestamp = db.Column(BIGINT(20, unsigned=True), onupdate=int(time.time()), comment='更新时间(时间戳)')
-    is_deleted = db.Column(TINYINT(3, unsigned=True), server_default=text('0'), comment='0正常;其他:已删除')
+    is_deleted = db.Column(BIGINT(20, unsigned=True), default=0, comment='0正常;其他:已删除')
     status = db.Column(TINYINT(3, unsigned=True), server_default=text('1'), comment='状态')
 
-    def keys(self):
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def get_columns(self):
         """
         返回所有字段对象
         :return:
         """
         return self.__table__.columns
 
-    def __getitem__(self, item):
-        return getattr(self, item)
-
-    def to_json(self, *args):
+    def get_fields(self):
         """
-        json转化
-        :param args: 不需要返回的字段列表
+        返回所有字段
+        :return:
+        """
+        return self.__dict__
+
+    def to_json(self, hidden_fields=None):
+        """
+        Json序列化
+        :param hidden_fields: 覆盖类属性 hidden_fields
         :return:
         """
 
+        hf = hidden_fields if hidden_fields and isinstance(hidden_fields, list) else self.hidden_fields
+
         model_json = {}
-        __dict = self.__dict__
 
-        for column in self.keys():
-            field = __dict.get(column.name)  # 获取字段名称
-            # print(column, type(column), type(field))
-            if isinstance(field, decimal.Decimal):  # Decimal -> float
-                field = round(float(field), 2)
-            elif isinstance(field, datetime):  # datetime -> str
-                field = str(field)
-            else:
-                pass
-            model_json.update({column.name: field})
+        for column in self.get_fields():
+            if column not in hf:  # 不需要返回的字段与值
+                if hasattr(self, column):
+                    field = getattr(self, column)
+                    if isinstance(field, decimal.Decimal):  # Decimal -> float
+                        field = round(float(field), 2)
+                    elif isinstance(field, datetime):  # datetime -> str
+                        field = str(field)
+                    model_json[column] = field
 
-        if args:
-            for skip_field in args:
-                if model_json.get(skip_field):
-                    del model_json[skip_field]
+        del model_json['_sa_instance_state']
 
         return model_json
 
@@ -102,39 +108,13 @@ class BaseModel(db.Model):
             db.session.rollback()
             raise TypeError('save_all error {}'.format(str(e)))
 
-    def update(self, *args, **kwargs):
-        """
-        更新
-        :param args: 不需要更新的字段列表 demo -> update(*["password"]) ...
-        :param kwargs: {字段:值} demo -> update(**{"name":"yyx"}) ...
-        :return:
-        """
-        if args:
-            args = list(args) + ['id', 'create_time']
-        else:
-            args = ['id', 'create_time']
-
-        for attr, value in kwargs.items():
-            # print(self, "【{}:{}】-【{}:{}】".format(attr, type(attr), value, type(value)))
-            if attr in self.__dict__.keys():
-                if attr not in args:
-                    new_value = json.dumps(value, ensure_ascii=False) if isinstance(value, dict) else str(value)
-                    setattr(self, attr, new_value)
-            else:
-                pass
-        try:
-            db.session.commit()
-        except BaseException as e:
-            db.session.rollback()
-            raise TypeError('update error {}'.format(str(e)))
-
     def delete(self):
         """
         逻辑删除
         :return:
         """
         try:
-            self.is_deleted = 2
+            self.is_deleted = self.id
             db.session.commit()
         except BaseException as e:
             db.session.rollback()
